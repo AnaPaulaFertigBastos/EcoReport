3d00889aaf1ca3c9ff52a9134afe7f2b4da21070
Ana Paula <anafertigbastos@gmail.com>
2026-03-13 16:19:56 -0300

    configuracoes

---
+++ EcoReport/EcoReport/Data/EcoReportContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using EcoReport.Models;
+
+namespace EcoReport.Data
+{
+    public class EcoReportContext : DbContext
+    {
+        public EcoReportContext(DbContextOptions<EcoReportContext> options) : base(options)
+        {
+        }
+
+    }
+}
--- EcoReport/EcoReport/Program.cs
+++ EcoReport/EcoReport/Program.cs
@@ -1,4 +1,13 @@
+using EcoReport.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("Postgres");
+
+builder.Services.AddDbContext<EcoReportContext>(options =>
+    options.UseNpgsql(connectionString)
+);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -12,6 +21,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
