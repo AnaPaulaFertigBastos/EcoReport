271415dfc821389fddd90a29935d95ce905440b3
Ana Paula <anafertigbastos@gmail.com>
2026-03-20 20:54:28 -0300

    mapa

--- EcoReport/EcoReport/wwwroot/js/site.js
+++ EcoReport/EcoReport/wwwroot/js/site.js
@@ -1,4 +1,36 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+var map = L.map('map').setView([-27.1, -52.6], 13);
 
-// Write your JavaScript code.
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap'
+}).addTo(map);
+
+navigator.geolocation.getCurrentPosition(function(position) {
+
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+
+    map.setView([lat, lng], 15);
+
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup("Sua localização")
+        .openPopup();
+
+});
+
+var marker;
+
+map.on('click', function(e) {
+
+    if(marker){
+        map.removeLayer(marker);
+    }
+
+    marker = L.marker(e.latlng).addTo(map);
+
+    console.log("Latitude:", e.latlng.lat);
+    console.log("Longitude:", e.latlng.lng);
+
+});
+
