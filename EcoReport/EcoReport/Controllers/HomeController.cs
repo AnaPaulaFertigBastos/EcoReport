87e81b72bc12e6b8f47a9fde630fc97048ebc198
Ana Paula <anafertigbastos@gmail.com>
2026-03-21 19:42:04 -0300

    tipos de area retornado index

--- EcoReport/EcoReport/Controllers/HomeController.cs
+++ EcoReport/EcoReport/Controllers/HomeController.cs
@@ -1,14 +1,30 @@
+using EcoReport.Data;
 using EcoReport.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 using System.Diagnostics;
 
 namespace EcoReport.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<HomeController> logger;
+        private readonly EcoReportContext _context;
+
+        public HomeController(ILogger<HomeController> logger, EcoReportContext context)
         {
-            return View();
+            this.logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var tiposDeArea = await _context.TipoDeArea
+                .OrderBy(t => t.Classificacao)
+                .ToListAsync();
+            //ViewData["TiposDeArea"] = tiposDeArea;
+            return View(tiposDeArea);
         }
 
         public IActionResult Privacy()
